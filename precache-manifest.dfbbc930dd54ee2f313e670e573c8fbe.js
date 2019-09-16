2defb05f84a8719fbfde6966802b7baf33e15126
VladyslavSak <vladyslav.sak@gmail.com>
2019-09-16 16:43:34 +0300

    Updates

---
+++ precache-manifest.dfbbc930dd54ee2f313e670e573c8fbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e0de9bcab7790b2533a6a1370bf1f83",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "afeee87f0defb4f5cf65",
+    "url": "/react_list-of-goods/static/css/main.4b41f5e5.chunk.css"
+  },
+  {
+    "revision": "b979ab577de52ef9f439",
+    "url": "/react_list-of-goods/static/js/2.5e6a8a20.chunk.js"
+  },
+  {
+    "revision": "afeee87f0defb4f5cf65",
+    "url": "/react_list-of-goods/static/js/main.25d6be8b.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
